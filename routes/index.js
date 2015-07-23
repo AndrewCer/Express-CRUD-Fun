10ce2ea25dac122aae399b13a66037cca4623628
Andrew Cer <cer.andrew@gmail.com>
2015-07-23 09:23:16 -0600

    basic environment set up

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+// var db = require('monk')('');
+// var filler = db.get('');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
